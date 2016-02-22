3f9746c826c78e1e57723ca97eba5ca93b589725
Maxxie <jyst7275@gmail.com>
2016-02-22 12:19:52 +0800

    bug fix: 404Bug debug=false todo 2\alipay 3\search

--- suiyuan/urls.py
+++ suiyuan/urls.py
@@ -23,9 +23,9 @@ from . import user_man
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index),
-    url(r'^news', views.news),
+    url(r'^news/$', views.news),
     url(r'^archives/([0-9]+)/([0-9]+)/([0-9]+)/([\w|\-]+)/', views.archives),
-    url(r'^about', views.about),
+    url(r'^about/$', views.about),
     url(r'^products/$', views.products),
     url(r'^products/archives/([\w|\-]+)/$', views.product_archives),
     url(r'^products/details/([\w|\-]+)/([\w|\-]+)/$', views.product_details),
@@ -41,12 +41,12 @@ urlpatterns = [
     url(r'^v1/user/address/location/', user_man.address_location),
     url(r'^v1/user/address/([0-9]+)/$', user_man.address_oper),
     url(r'^v1/user/sendcode/([0-9]+)/$', user_man.user_code_gen),
-    url(r'^contact', views.contact),
-    url(r'^people', views.people),
+    url(r'^contact/$', views.contact),
+    url(r'^people/$', views.people),
     url(r'^summernote/', include('django_summernote.urls')),
     url(r'^v1/passage/([\w]+)/$', rest.PassageDetail.as_view()),
     url(r'^v1/qrcode/(.+)$', views.qrcode),
-    url(r'search', views.search)
+    url(r'search$', views.search)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler500 = 'suiyuan.views.my_500_view'
